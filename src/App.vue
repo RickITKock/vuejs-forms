510054823c6716030bbfc884ed92d40ea5e7d8f4
rick124 <rick@roosh.online>
2023-02-01 13:51:29 +0100

    Create form and add form-validation

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
 </template>
 
 <script>
-import TheForm from './components/TheForm.vue';
+import TheForm from "./components/TheForm.vue";
 
 export default {
   components: {
-    TheForm
-  }  
-}
+    TheForm,
+  },
+};
 </script>
 
 <style>
